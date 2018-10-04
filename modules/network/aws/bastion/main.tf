c68565e905ea095f6ea4b4ba664e7d2196f9b854
Rob Morgan <robbym@gmail.com>
2018-10-04 15:09:25 +0200

    allow the bastion node to connect to the internet

--- modules/network/aws/bastion/main.tf
+++ modules/network/aws/bastion/main.tf
@@ -65,6 +65,16 @@ resource "aws_security_group_rule" "allow_ssh_inbound" {
   security_group_id = "${aws_security_group.bastion.id}"
 }
 
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = "${aws_security_group.bastion.id}"
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # LOOKUP THE LATEST UBUNTU 16.04 AMI
 # ---------------------------------------------------------------------------------------------------------------------
