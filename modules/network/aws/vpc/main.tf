2e077ed5cf3a72cac3577f4e3f2f4b1ec6656d0d
Rob Morgan <robbym@gmail.com>
2018-10-05 11:01:19 +0200

    actually create nat gateways

--- modules/network/aws/vpc/main.tf
+++ modules/network/aws/vpc/main.tf
@@ -89,6 +89,7 @@ resource "aws_eip" "nat" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_nat_gateway" "main" {
+  count         = "${length(var.private_subnets)}"
   allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
   depends_on    = ["aws_internet_gateway.main"]
