a51855d21dab7a0eb9c384781c959de5ae11e1f8
Rob Morgan <robbym@gmail.com>
2018-10-10 18:39:56 +0200

    lock providers

--- examples/magecloudkit-simple-vpc/main.tf
+++ examples/magecloudkit-simple-vpc/main.tf
@@ -1,12 +1,21 @@
 # ---------------------------------------------------------------------------------------------------------------------
-# CONFIGURE THE AWS PROVIDER
+# CONFIGURE THE PROVIDERS
 # ---------------------------------------------------------------------------------------------------------------------
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region  = "${var.aws_region}"
+  version = "~> 1.39"
 }
 
-# Require a modern version of Terraform so we have access to the recent features.
+provider "template" {
+  version = "~> 1.0"
+}
+
+provider "random" {
+  version = "~> 2.0"
+}
+
+# Require a modern version of Terraform so we have access to the recent features like locals.
 terraform {
   required_version = ">= 0.10.3"
 }
