7a8a5dae4ce1838c77bf6cd0d148df446eca534a
Rob Morgan <robbym@gmail.com>
2018-10-12 10:29:01 +0200

    configure remote state

--- main.tf
+++ main.tf
@@ -6,7 +6,14 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-# Require a modern version of Terraform so we have access to the recent features.
 terraform {
+  # Require a modern version of Terraform so we have access to the recent features.
   required_version = ">= 0.10.3"
+
+  # Also configure remote state for working in teams.
+  backend "s3" {
+    bucket = "brightfame-state"
+    key    = "terraform"
+    region = "us-west-1"
+  }
 }
