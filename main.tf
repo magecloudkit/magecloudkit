35ce604a6664f6402436ceebddb29bcd35b147e2
Rob Morgan <robbym@gmail.com>
2018-10-15 13:55:03 +0200

    make sure kiwico state doesnt interfere with the tests

--- main.tf
+++ main.tf
@@ -10,15 +10,3 @@ provider "aws" {
 provider "template" {
   version = "~> 1.0"
 }
-
-terraform {
-  # Require a modern version of Terraform so we have access to the recent features.
-  required_version = ">= 0.10.3"
-
-  # Also configure remote state for working in teams.
-  backend "s3" {
-    bucket = "kiwico-state"
-    key    = "env/production/terraform.tfstate"
-    region = "us-west-1"
-  }
-}
