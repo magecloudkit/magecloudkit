5ca6a4d41dd1038d1d28d8582425e9c3f1359c1f
Rob Morgan <robbym@gmail.com>
2018-10-15 09:37:33 +0200

    make sure the cluster arn is returned

--- modules/app-cluster/aws/ecs-cluster/outputs.tf
+++ modules/app-cluster/aws/ecs-cluster/outputs.tf
@@ -2,8 +2,8 @@ output "cluster_id" {
   value = "${aws_ecs_cluster.ecs_cluster.id}"
 }
 
-output "cluster_name" {
-  value = "${aws_ecs_cluster.ecs_cluster.cluster_name}"
+output "cluster_arn" {
+  value = "${aws_ecs_cluster.ecs_cluster.arn}"
 }
 
 output "asg_name" {
