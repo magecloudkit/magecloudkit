e8620936764c0533330bce367780a65af4992794
Rob Morgan <robbym@gmail.com>
2018-10-15 09:40:09 +0200

    output cluster endpoint

--- modules/database/aws/aurora/outputs.tf
+++ modules/database/aws/aurora/outputs.tf
@@ -6,6 +6,10 @@ output "db_rds_cluster_instances" {
   value = "${aws_rds_cluster_instance.cluster_instances.*.id}"
 }
 
+output "endpoint" {
+  value = "${aws_rds_cluster.default.endpoint}"
+}
+
 output "security_group_id" {
   value = "${aws_security_group.rds.id}"
 }
