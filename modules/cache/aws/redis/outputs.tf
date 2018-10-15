b1bbf03c8509915ead15e2a272c47b3e9cd1c911
Rob Morgan <robbym@gmail.com>
2018-10-15 09:38:11 +0200

    return the redis primary address

--- modules/cache/aws/redis/outputs.tf
+++ modules/cache/aws/redis/outputs.tf
@@ -5,3 +5,7 @@ output "redis_port" {
 output "security_group_id" {
   value = "${aws_security_group.redis.id}"
 }
+
+output "primary_address" {
+  value = "${aws_elasticache_cluster.redis.cache_nodes.0.address}"
+}
