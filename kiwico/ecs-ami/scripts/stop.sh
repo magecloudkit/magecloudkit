7778a169bc45abbf5b6c86ac501df87d1df71692
Rob Morgan <robbym@gmail.com>
2018-10-16 14:39:18 +0200

    add kiwico ecs ami

---
+++ kiwico/ecs-ami/scripts/stop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ "$(ls -A /tmp/root)" ]
+then
+  cd /tmp/root
+  cp -r * /
+fi
+
+systemctl daemon-reload
+
+apt-get -y autoremove
+apt-get -y autoclean
+
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+rm -rf $HOME/.ssh/authorized_keys
+
+for f in $(find /var/log -type f) ; do
+  dd if=/dev/null of=$f
+done
