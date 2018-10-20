c0fae0b29bcac6929f1b80f2fbd18741bd10dafc
Rob Morgan <robbym@gmail.com>
2018-10-19 18:09:38 -0700

    only allow access to the app servers from the bastion instance

--- app-cluster.tf
+++ app-cluster.tf
@@ -18,15 +18,11 @@ module "app_cluster" {
   max_size         = 6
   desired_capacity = 2
 
-  # To make testing easier, we allow SSH requests from any IP address here. In a production deployment, we strongly
-  # recommend you limit this to the IP address ranges of known, trusted servers.
-  allowed_ssh_cidr_blocks = ["0.0.0.0/0"]
-
   # Allow inbound SSH access from the Bastion instance
-  #allowed_ssh_security_group_ids = ["${module.bastion.security_group_id}"]
-  #allowed_ssh_security_group_ids = ["${aws_security_group.bastion.id}"]
+  allowed_ssh_security_group_ids = ["${module.bastion.security_group_id}"]
 
   key_pair_name = "${var.key_pair_name}"
+
   # We recommend using a separate EBS Volume for the Docker data dir
   ebs_block_devices = [
     {
@@ -35,6 +31,7 @@ module "app_cluster" {
       volume_size = 50
     },
   ]
+
   # Set custom tags
   tags = [
     {
