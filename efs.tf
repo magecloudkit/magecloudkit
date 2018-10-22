5e0d915f8e38c94bfd1839fbb5d956d69c513c96
Rob Morgan <robbym@gmail.com>
2018-10-21 17:07:20 -0700

    improve docs

--- efs.tf
+++ efs.tf
@@ -14,7 +14,7 @@ module "efs" {
   allowed_inbound_security_group_count = 1
   allowed_inbound_security_group_ids   = ["${module.app_cluster.security_group_id}"]
 
-  # An example of custom tags
+  # Set custom tags
   tags = [
     {
       Environment = "${var.environment}"
