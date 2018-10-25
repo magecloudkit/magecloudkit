3a43fc29231176f5a33f305671757e83b9dd0687
Rob Morgan <robbym@gmail.com>
2018-10-25 15:54:40 -0700

    add logs group for admin

--- logs.tf
+++ logs.tf
@@ -1,11 +1,18 @@
 # ---------------------------------------------------------------------------------------------------------------------
-# CREATE THE LOGS GROUP
+# CREATE THE LOG GROUPS
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "ecs-cluster-logs" {
   source = "./modules/monitoring/aws/logs"
 
-  name              = "${var.ecs_cluster_name}"
+  name              = "${var.ecs_cluster_name_app}"
+  retention_in_days = 30
+}
+
+module "ecs-cluster-logs-admin" {
+  source = "./modules/monitoring/aws/logs"
+
+  name              = "${var.ecs_cluster_name_admin}"
   retention_in_days = 30
 }
 
