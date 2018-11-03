35242e783b110fdebe1092f279f6ea0eadd2a930
Rob Morgan <robbym@gmail.com>
2018-11-03 18:43:06 +0100

    add todo

--- kiwico/bin/deploy.sh
+++ kiwico/bin/deploy.sh
@@ -7,3 +7,5 @@ BUILD_ID=165
 
 $DIR/ecs-deploy.sh -c production-admin -n admin-service -to ${BUILD_ID} -i ignore -t 360
 $DIR/ecs-deploy.sh -c production-app -n web-service -to ${BUILD_ID} -i ignore -t 360
+
+# TODO - something for Jenkins
