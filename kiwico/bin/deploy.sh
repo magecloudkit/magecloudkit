111a126a9c044455662f5da9d947029b20634795
Rob Morgan <robbym@gmail.com>
2018-11-03 18:38:23 +0100

    add kiwico deploy scripts

---
+++ kiwico/bin/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+BUILD_ID=165
+
+$DIR/ecs-deploy.sh -c production-admin -n admin-service -to ${BUILD_ID} -i ignore -t 360
+$DIR/ecs-deploy.sh -c production-app -n web-service -to ${BUILD_ID} -i ignore -t 360
