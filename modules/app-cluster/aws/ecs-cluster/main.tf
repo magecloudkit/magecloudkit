796c61997a7f0571acdfe0e90031cde4774afecb
Rob Morgan <robbym@gmail.com>
2018-11-23 18:36:11 +0100

    fix ecs instance draining during asg update

--- modules/app-cluster/aws/ecs-cluster/main.tf
+++ modules/app-cluster/aws/ecs-cluster/main.tf
@@ -22,13 +22,11 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 resource "aws_autoscaling_group" "autoscaling_group" {
   name = "${var.cluster_name}-${aws_launch_configuration.launch_configuration.name}"
 
-  launch_configuration = "${aws_launch_configuration.launch_configuration.id}"
-  vpc_zone_identifier  = ["${var.subnet_ids}"]
-
-  min_size             = "${var.min_size}"
-  max_size             = "${var.max_size}"
-  termination_policies = ["${var.termination_policies}"]
-
+  launch_configuration      = "${aws_launch_configuration.launch_configuration.id}"
+  vpc_zone_identifier       = ["${var.subnet_ids}"]
+  min_size                  = "${var.min_size}"
+  max_size                  = "${var.max_size}"
+  termination_policies      = ["${var.termination_policies}"]
   health_check_type         = "${var.health_check_type}"
   health_check_grace_period = "${var.health_check_grace_period}"
   wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
--- modules/app-cluster/aws/ecs-instance-draining/variables.tf
+++ modules/app-cluster/aws/ecs-instance-draining/variables.tf
@@ -7,3 +7,13 @@ variable "name" {
   type        = "string"
   description = "A preferably short unique identifier for this module"
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters are preconfigured and have reasonable default values.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "retention_in_days" {
+  description = "The number of days for log retention. We recommend a value greater than 7."
+  default     = 30
+}
